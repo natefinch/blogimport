3ac245694268aa525f00607caccecb39bd695d68
atulsingh0 <victimizeit@gmail.com>
2023-02-17 22:55:22 -0500

    add-date-filename | added date to output filename

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/xml"
 	"flag"
 	"fmt"
-	"log"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,8 +13,6 @@ import (
 	"time"
 	"unicode"
 )
-
-
 
 type Date time.Time
 
@@ -24,54 +22,54 @@ func (d Date) String() string {
 
 func (d *Date) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
 	var v string
-    dec.DecodeElement(&v, &start)
-    t, err := time.Parse("2006-01-02T15:04:05.000-07:00", v)
-    if err != nil {
-    	return err
-    }
-    *d = Date(t)
-    return nil
+	dec.DecodeElement(&v, &start)
+	t, err := time.Parse("2006-01-02T15:04:05.000-07:00", v)
+	if err != nil {
+		return err
+	}
+	*d = Date(t)
+	return nil
 }
 
-type Draft bool 
+type Draft bool
 
 func (d *Draft) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
 	var v string
-    dec.DecodeElement(&v, &start)
-    switch v {
-    case "yes":
-    	*d = true
-    	return nil
-    case "no":
-    	*d = false
-    	return nil
-    }
-    return fmt.Errorf("Unknown value for draft boolean: %s", v)
+	dec.DecodeElement(&v, &start)
+	switch v {
+	case "yes":
+		*d = true
+		return nil
+	case "no":
+		*d = false
+		return nil
+	}
+	return fmt.Errorf("Unknown value for draft boolean: %s", v)
 }
 
 type Author struct {
 	Name string `xml:"name"`
-	Uri string `xml:"uri"`
+	Uri  string `xml:"uri"`
 }
 
 type Export struct {
 	XMLName xml.Name `xml:"feed"`
-	Entries []Entry `xml:"entry"`
+	Entries []Entry  `xml:"entry"`
 }
 
 type Entry struct {
-	ID string `xml:"id"`
-	Published Date `xml:"published"`
-	Updated Date `xml:"updated"`
-	Draft Draft `xml:"control>draft"`
-	Title string `xml:"title"`
-	Content string `xml:"content"`
-	Tags Tags `xml:"category"`
-	Author Author `xml:"author"`
-	Extra string
+	ID        string `xml:"id"`
+	Published Date   `xml:"published"`
+	Updated   Date   `xml:"updated"`
+	Draft     Draft  `xml:"control>draft"`
+	Title     string `xml:"title"`
+	Content   string `xml:"content"`
+	Tags      Tags   `xml:"category"`
+	Author    Author `xml:"author"`
+	Extra     string
 }
 type Tag struct {
-	Name string `xml:"term,attr"`
+	Name   string `xml:"term,attr"`
 	Scheme string `xml:"scheme,attr"`
 }
 
@@ -130,10 +128,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if !info.IsDir(){
+	if !info.IsDir() {
 		log.Fatal("Second argument is not a directory.")
- 	}
-
+	}
 
 	b, err := ioutil.ReadFile(args[0])
 	if err != nil {
@@ -174,7 +171,7 @@ func main() {
 		if entry.Draft {
 			drafts++
 		} else {
-			count++		
+			count++
 		}
 	}
 	log.Printf("Wrote %d published posts to disk.", count)
@@ -184,8 +181,8 @@ func main() {
 var delim = []byte("+++\n")
 
 func writeEntry(e Entry, dir string) error {
-	filename := filepath.Join(dir, makePath(e.Title)+".md")
-	f, err := os.OpenFile(filename, os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0644)
+	filename := filepath.Join(dir, makePath(e.Published, e.Title)+".md")
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -194,11 +191,10 @@ func writeEntry(e Entry, dir string) error {
 	return t.Execute(f, e)
 }
 
-
 // Take a string with any characters and replace it so the string could be used in a path.
 // E.g. Social Media -> social-media
-func makePath(s string) string {
-	return unicodeSanitize(strings.ToLower(strings.Replace(strings.TrimSpace(s), " ", "-", -1)))
+func makePath(d Date, s string) string {
+	return fmt.Sprintf("%v-%s", d.String()[:10], unicodeSanitize(strings.ToLower(strings.Replace(strings.TrimSpace(s), " ", "-", -1))))
 }
 
 func unicodeSanitize(s string) string {
@@ -210,13 +206,5 @@ func unicodeSanitize(s string) string {
 			target = append(target, r)
 		}
 	}
-
 	return string(target)
 }
-
-
-
-
-
-
-
